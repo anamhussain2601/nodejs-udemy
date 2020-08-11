761e9181bde16f0fb1a3ac70f550a5af92c3580a
qainfotech <anamhussain2601@gmail.com>
2020-08-11 18:16:54 +0530

    Update operation

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -19,40 +19,17 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    // db.collection('users').findOne({name: 'Jane'}, (error, user)=>{
-
-
-    //     if(error) {
-    //         console.log('Unable to fetch!')
-    //     }
-    //     console.log(user)
-
-    // })
-
-    db.collection('users').find({ age: '26' }).toArray((error, users) => {
-        console.log(users)
-    })
-
-    db.collection('users').find({ age: '26' }).count((error, users) => {
-        console.log(users)
-    })
-
-
-
-    db.collection('tasks').findOne({_id: new Object('5f3171320ad87816c52192fc')}, (error, task)=>{
-        if(error) {
-            console.log('Unable to fetch!')
+    const updatePromise = db.collection('users').updateOne({
+        _id: new Object('5f3171b866a195171d11c658')
+    },{
+        $set:{
+            name: 'Joe'
         }
-        console.log(task)
-
-    })
-    
-
-    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
-        if (error) {
-            console.log('Unable to fetch!')
-        }
-        console.log(tasks,'tasks')
     })
 
+    updatePromise.then((result)=>{
+        console.log(result)
+    }).catch((error)=>{
+        console.log(error)
+    })
 })
