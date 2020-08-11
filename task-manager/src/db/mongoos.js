86c0863cd5079538239c4d3d47b2ca63637045dc
qainfotech <anamhussain2601@gmail.com>
2020-08-11 22:14:02 +0530

    data validation

--- task-manager/src/db/mongoos.js
+++ task-manager/src/db/mongoos.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser: true,
@@ -9,17 +10,36 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
 //defining a model
 const User = mongoose.model('User',{
     name: {
-        type : String
+        type : String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Invalid Email!')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        validate(value) {
+            if(value<0) {
+                throw new Error('Age must be a positive number!')
+            }
+        }
     }
 })
 
 //creating an instance of the above model
 const me = new User({
-    name: 'Jane',
-    age: 'doe'
+   name: '   Monica',
+   age: 24,
+   email: 'MONICA@gmail.com'
 })
 
 
@@ -29,3 +49,29 @@ me.save().then(()=>{
 }).catch((error)=>{
     console.log('Error!', error)
 })
+
+
+//****************************************** */
+//defining a Task model
+// const User = mongoose.model('Task',{
+//     name: {
+//         type : String
+//     },
+//     age: {
+//         type: Number
+//     }
+// })
+
+// //creating an instance of the above model
+// const me = new User({
+//     description: 'Jane',
+//     completed: false
+// })
+
+
+// //saving the instance to the db 
+// me.save().then(()=>{
+//     console.log(me)
+// }).catch((error)=>{
+//     console.log('Error!', error)
+// })
