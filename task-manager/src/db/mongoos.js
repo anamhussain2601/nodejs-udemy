c044a7235f929cd860fd7648a6e1d2b545de91fe
qainfotech <anamhussain2601@gmail.com>
2020-08-13 23:08:54 +0530

    Creation of task model

--- task-manager/src/db/mongoos.js
+++ task-manager/src/db/mongoos.js
@@ -1,21 +1,8 @@
 const mongoose = require('mongoose')
 
+
+//mongoose connets to the db.
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser: true,
     useCreateIndex: true
 })
-
-
-
-
-
-//****************************************** */
-//defining a Task model
-// const User = mongoose.model('Task',{
-//     name: {
-//         type : String
-//     },
-//     age: {
-//         type: Number
-//     }
-// })
