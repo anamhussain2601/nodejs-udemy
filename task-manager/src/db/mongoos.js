eda0db4a4e8ee6a7fabc54aa80a325d42a4f8ae4
qainfotech <anamhussain2601@gmail.com>
2020-08-13 22:56:01 +0530

    Proper modelling done with the instanstallation express and validator

--- task-manager/src/db/mongoos.js
+++ task-manager/src/db/mongoos.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser: true,
@@ -7,60 +6,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
 })
 
 
-//defining a model
-const User = mongoose.model('User',{
-    name: {
-        type : String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if(!validator.isEmail(value)) {
-                throw new Error('Invalid Email!')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6,
-        trim: true,
-        validate(value) {
-            if(value.includes('password')) {
-                throw new Error('Invalid Password')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        validate(value) {
-            if(value<0) {
-                throw new Error('Age must be a positive number!')
-            }
-        }
-    }
-})
 
-//creating an instance of the above model
-const me = new User({
-   name: '   Monica',
-   age: 24,
-   email: 'MONICA@gmail.com',
-   password: 'abcdef0123!'
-})
-
-
-//saving the instance to the db 
-me.save().then(()=>{
-    console.log(me)
-}).catch((error)=>{
-    console.log('Error!', error)
-})
 
 
 //****************************************** */
@@ -72,18 +18,4 @@ me.save().then(()=>{
 //     age: {
 //         type: Number
 //     }
-// })
-
-// //creating an instance of the above model
-// const me = new User({
-//     description: 'Jane',
-//     completed: false
-// })
-
-
-// //saving the instance to the db 
-// me.save().then(()=>{
-//     console.log(me)
-// }).catch((error)=>{
-//     console.log('Error!', error)
 // })
