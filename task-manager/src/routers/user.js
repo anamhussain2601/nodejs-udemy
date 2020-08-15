51f18a694a1f15098153b07e17397f8f79542749
qainfotech <anamhussain2601@gmail.com>
2020-08-15 22:46:07 +0530

    logging user

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -2,11 +2,6 @@ const express = require('express')
 const router = new express.Router()
 const User = require('../models/user')
 
-router.get('/test', (req,res)=>{
-    res.send('From new file')
-})
-
-
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -16,10 +11,20 @@ router.post('/users', async (req, res) => {
         res.status(201).send(user)
 
     }
-    catch(e) {
+    catch (e) {
         res.status(400).send(e)
     }
 
+})
+
+router.post('/users/login', async (req,res)=>{
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+
+        res.send(user)
+    }catch(e) {
+        res.status(400).send()
+    }
 })
 
 router.get('/users', async (req, res) => {
@@ -28,71 +33,71 @@ router.get('/users', async (req, res) => {
         const users = await User.find({})
         res.send(users)
     }
-    catch(e) {
+    catch (e) {
         res.status(500).send()
     }
 
 })
 
-router.patch('/users/:id', async(req,res)=>{
+router.patch('/users/:id', async (req, res) => {
 
     const updates = Object.keys(req.body)
-    const allowedUpdates = ['name','email', 'password', 'age']
-    const isValidaOperation = updates.every((update)=>{
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidaOperation = updates.every((update) => {
         return allowedUpdates.includes(update)
     })
 
-    if(!isValidaOperation) {
-        return res.status(400).send({error: 'Invalid update'})
+    if (!isValidaOperation) {
+        return res.status(400).send({ error: 'Invalid update' })
     }
     try {
         const user = await User.findById(req.params.id)
 
-        updates.forEach((update)=>{
+        updates.forEach((update) => {
             user[update] = req.body[update]
         })
 
         await user.save()
         // const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true})
 
-        if( !user) {
+        if (!user) {
             return res.status(404).send()
         }
 
         res.send(user)
     } catch (e) {
-            res.status(400).send(e)
+        res.status(400).send(e)
     }
 })
 
-router.get('/users/:id', async(req, res) => {
+router.get('/users/:id', async (req, res) => {
     const _id = req.params.id
 
 
     try {
         const user = await User.findById(_id)
-        if( !user ) {
+        if (!user) {
             return res.status(404).send()
         }
         res.send(user)
     }
-    catch(e) {
+    catch (e) {
         res.status(500).send()
     }
 
 })
 
 
-router.delete('/users/:id', async (req,res)=>{
+router.delete('/users/:id', async (req, res) => {
     try {
         const user = await User.findByIdAndDelete(req.params.id)
 
-        if(!user) {
+        if (!user) {
             return res.status(404).send()
         }
         res.send(user)
 
-    }catch(e) {
+    } catch (e) {
         res.status(500).send()
     }
 })
