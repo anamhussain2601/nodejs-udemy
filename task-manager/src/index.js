991d3d4fd63a70671eb575760c5341b552a5cb5e
qainfotech <anamhussain2601@gmail.com>
2020-08-30 01:31:26 +0530

    Multer: Adding suppport for file upload

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,40 +6,24 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 8000
 
-// app.use((req,res,next)=>{
-//     if(req.method === 'GET') {
-//         res.send('GET request are disabled')
-//     }else {
-//         next()  // route hanlders work as it is
-//     }
-// })
+const multer = require('multer')
+const upload = multer({
+    dest: 'images'
+})
 
-// app.use((req,res,next)=>{
-//     res.status(503).send('Site is currently down. check back soon')
-// })
+//server is configured to accept and save file that uploaded to it
+app.post('/upload',upload.single('upload'), (req,res) => {
+    res.send()
+})
 
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
 
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-    // const task = await Task.findById('5f4a1c480887627d0c740e75')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    // const user = await User.findById('5f4a187d839cff780b3e6963')
-    // await user.populate('tasks').execPopulate()
-    // console.log(user.tasks)
-}
-
-main()
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -3,6 +3,8 @@ const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
 
+const multer = require('multer')
+
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
@@ -115,6 +117,19 @@ router.delete('/users/me',auth, async (req, res) => {
         res.send(req.user)
     } catch (e) {
         res.status(500).send()
+    }
+})
+
+const upload = multer({
+    dest: 'avatars'
+})
+
+
+router.post('/users/me/avatar',upload.single('avatar'), async(req,res)=>{
+    try{
+        res.send()
+    }catch(e){
+        req.status(400).send()
     }
 })
 
