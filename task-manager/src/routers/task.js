e484e0941b284bbb3192cbc273e3b90f8aca8c21
qainfotech <anamhussain2601@gmail.com>
2020-08-29 16:52:05 +0530

    Authenticating task end points

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -34,6 +34,23 @@ router.post('/tasks',auth, async(req, res) => {
     // })
 })
 
+
+router.get('/tasks/:id', auth, async (req, res)=>{
+    const _id =  req.params.id
+
+    try {
+        const task = await Task.findOne({_id, owner: req.user._id})
+
+        if(!task) {
+            return res.status(404).send
+        }
+
+        res.send(task)
+    }catch(e) {
+        res.send(500).send()
+    }
+})
+
 router.patch('/task/:id', async (req,res)=>{
 
     const updates = Object.keys(req.body)
