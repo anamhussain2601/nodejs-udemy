fcd2eeccdd131683e9abfa327eab35abc1ec4540
qainfotech <anamhussain2601@gmail.com>
2020-08-30 16:00:46 +0530

    adding imaged to user profile

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -121,7 +121,6 @@ router.delete('/users/me',auth, async (req, res) => {
 })
 
 const upload = multer({
-    dest: 'avatars',
     limits: {
         fileSize:1000000
     },
@@ -135,16 +134,26 @@ const upload = multer({
 })
 
 
-router.post('/users/me/avatar',upload.single('avatar'), async(req,res)=>{
+router.post('/users/me/avatar',auth, upload.single('avatar'), async(req,res)=>{
     // try{
     //     res.send()
     // }catch(e){
     //     req.status(400).send()
     // }
 
+    req.user.avatar = req.file.buffer
+    await req.user.save()
+
     res.send()
 },(error, req,res,next) => {
     res.status(400).send({error: error.message})
 })
 
+router.delete('/users/me/avatar',auth, async(req,res)=>{
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+},(error, req,res,next) => {
+    res.status(400).send({error: error.message})
+})
 module.exports = router
