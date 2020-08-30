58c801cbfef455b1b7861c5b89a2cfd4e4191554
qainfotech <anamhussain2601@gmail.com>
2020-08-30 14:36:10 +0530

    validation of files

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -121,7 +121,17 @@ router.delete('/users/me',auth, async (req, res) => {
 })
 
 const upload = multer({
-    dest: 'avatars'
+    dest: 'avatars',
+    limits: {
+        fileSize:1000000
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload and image'))
+        }
+
+        cb(undefined, true)
+    }
 })
 
 
